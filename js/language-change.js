4e7101b2ff5ba30a90101b655a68c1c6df323792
Hector Franco <tor6.franco6@hotmail.com>
2019-03-30 15:06:23 -0600

    Translated body page (EN/ES)

---
+++ js/language-change.js
@@ -0,0 +1,17 @@
+function changeLenguage (toLanguage) {
+  if (toLanguage === 'es') {
+    document.getElementById('english-notice').style.display = 'none'
+    document.getElementById('spanish-notice').style.display = 'block'
+    document.getElementById('startbutton').innerHTML = 'Siguiente generación'
+    document.getElementById('automaticButton').innerHTML = 'Automático'
+    document.getElementById('pauseButton').innerHTML = 'Pausa'
+    document.getElementById('startGameOfLifeButton').innerHTML = 'Iniciar Juego De La Vida'
+  } else {
+    document.getElementById('english-notice').style.display = 'block'
+    document.getElementById('spanish-notice').style.display = 'none'
+    document.getElementById('startbutton').innerHTML = 'Next generation'
+    document.getElementById('automaticButton').innerHTML = 'Automatic'
+    document.getElementById('pauseButton').innerHTML = 'Pause'
+    document.getElementById('startGameOfLifeButton').innerHTML = 'Start Game Of Life'
+  }
+}
